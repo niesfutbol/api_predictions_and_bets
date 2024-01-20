fd5dca4af2d27435540257a819acf1b70b73bc3d
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-01-19 22:29:15 -0800

    🚧 Clean data

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
-import json
 import pandas as pd
 
-premier_league = pd.read_csv("/app/data/predictions_39_2023_0.csv")
-serie_a = pd.read_csv("/app/data/predictions_135_2023_0.csv")
-bets = pd.concat([premier_league, serie_a], ignore_index=True).sort_values(by=['date'])
-
+def read_data():
+    premier_league = pd.read_csv("/app/data/predictions_39_2023_0.csv")
+    busdesliga = pd.read_csv("/app/data/predictions_78_2023_0.csv")
+    serie_a = pd.read_csv("/app/data/predictions_135_2023_0.csv")
+    all_bets = pd.concat([premier_league, serie_a, busdesliga], ignore_index=True).sort_values(by=['date'])
+    return all_bets.loc[(all_bets["home"] > 0.55) | (all_bets["away"] > 0.55) | (all_bets["draw"] > 0.55)]
 app = FastAPI()
 
 
 @app.get("/v1/predictions")
 def get_predictions():
+    bets = read_data()
     return bets.to_dict(orient="records")
