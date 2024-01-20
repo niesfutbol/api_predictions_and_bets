c66dbf13827d4e6cc1d90ea00a9d54f064517b71
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-01-19 21:16:22 -0800

    🪲 Fix path

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 import json
 import pandas as pd
 
-premier_league = pd.read_csv("./data/predictions_39_2023_0.csv.csv")
-serie_a = pd.read_csv("./data/predictions_135_2023_0.csv.csv")
+premier_league = pd.read_csv("/app/data/predictions_39_2023_0.csv")
+serie_a = pd.read_csv("/app/data/predictions_135_2023_0.csv")
 bets = pd.concat([premier_league, serie_a], ignore_index=True).sort_values(by=['date'])
 
 app = FastAPI()
